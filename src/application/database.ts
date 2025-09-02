8573f0f9458c0a6a5adf38935a8bb8470ed043c4
Yaser Syafa <syafayaser@gmail.com>
2025-09-02 12:41:08 +0700

    feat(prisma): update prisma import path

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { logger } from "./logging";
 
 export const prismaClient = new PrismaClient({
